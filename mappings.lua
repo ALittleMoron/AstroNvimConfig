7cfe98cc0dcc55de5c289ae1584b20e2be12f623
Dmitriy Lunev <dima.lunev14@gmail.com>
2023-06-08 14:44:39 +0300

    abc

--- mappings.lua
+++ mappings.lua
@@ -6,22 +6,38 @@ return {
     ["<leader><leader>w"] = {
       function ()
         hop.hint_words({ direction = directions.AFTER_CURSOR, current_line_only = false })
-      end
+      end,
+      desc = "Jump ahead to word start",
     },
-    ["<leader><leader>W"] = {
+    ["<leader><leader>b"] = {
       function ()
         hop.hint_words({ direction = directions.BEFORE_CURSOR, current_line_only = false })
-      end
+      end,
+      desc = "Jump back to word start",
     },
     ["<leader><leader>s"] = {
       function ()
         hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
-      end
+      end,
+      desc = "Jump ahead to specific char",
     },
     ["<leader><leader>S"] = {
       function ()
         hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
-      end
+      end,
+      desc = "Jump back to specific char",
+    },
+    ["<leader><leader>p"] = {
+      function ()
+        hop.hint_patterns({ direction = directions.AFTER_CURSOR, current_line_only = false })
+      end,
+      desc = "Jump ahead to pattern",
+    },
+    ["<leader><leader>P"] = {
+      function ()
+        hop.hint_patterns({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+      end,
+      desc = "Jump back to pattern",
     },
   },
   t = {},
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,14 +1,14 @@
 return {
-     {
-	    "ellisonleao/gruvbox.nvim",
-	    priority = 1000,
-     },
-     {
-       "phaazon/hop.nvim",
-       branch = "v2",
-       config = function()
-         require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
-       end
-     },
+  {
+	  "ellisonleao/gruvbox.nvim",
+	  priority = 1000,
+  },
+  {
+     "phaazon/hop.nvim",
+     branch = "v2",
+     config = function()
+       require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
+     end
+  },
 }
 
